73c6aeb036200af4f540ec07e7ebf2021a531f67
nchampel <nico.champel@gmail.com>
2026-09-23 18:07:10 +0200

    feat: 23/9/26 mise en place radar avec localisation et ressources début

---
+++ src/Controller/ApiGPSController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiGPSController extends AbstractController
+{
+    #[Route('/api/gps', name: 'app_api_gps')]
+    public function index(): Response
+    {
+        // return $this->json([
+        //     [
+        //         "id" => 1,
+        //         "latitude" => 43.5297,
+        //         "longitude" => 5.4474,
+        //         "activationRadius" => 50,
+        //         "imageUrl" => "https://...",
+        //         "type" => "wood"
+        //         ],
+        //     ]);
+        
+    return $this->json([
+        [
+        "id" => 1,
+        "latitude" => 43.52975,
+        "longitude" => 5.44740,
+        "activationRadius" => 3,
+        "type" => "wood"
+    ],
+    [
+        "id" => 2,
+        "latitude" => 43.52985,
+        "longitude" => 5.44745,
+        "activationRadius" => 3,
+        "type" => "water"
+    ],
+        [
+        "id" => 3,
+        "latitude" => 37.4219982,
+        "longitude" => -122.084,
+        "activationRadius" => 3,
+        "type" => "wood"
+    ],
+    [
+        "id" => 4,
+        "latitude" => 37.4219984,
+        "longitude" => -122.084,
+        "activationRadius" => 3,
+        "type" => "water"
+    ],
+        [
+        "id" => 3,
+        "latitude" => 43.4237381,
+        "longitude" => 5.2873262,
+        "activationRadius" => 3,
+        "type" => "wood"
+    ],
+    [
+        "id" => 4,
+        "latitude" => 43.4237382,
+        "longitude" => 5.2873261,
+        "activationRadius" => 3,
+        "type" => "water"
+    ],
+    ]);
+        
+    }
+}
