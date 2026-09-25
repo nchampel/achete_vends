77a6ef539a51e0b1e66e6c4146c119ec2362c2b6
nchampel <nico.champel@gmail.com>
2026-09-25 21:46:57 +0200

    feat: 25/9/26 continuation gps radar resource bâtiment 8

--- src/Controller/ApiGPSController.php
+++ src/Controller/ApiGPSController.php
@@ -101,7 +101,7 @@ class ApiGPSController extends AbstractController
     [
         "id" => 9,
         "latitude" => 43.4237402,
-        "longitude" => 5.2874569,
+        "longitude" => 5.2875569,
         "activationRadius" => 3,
         "type" => "water"
     ],
